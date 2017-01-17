2ecb421cba84d59c5509674396e39e4d0a1132c6
wtung008 <wayne.h.tung@gmail.com>
2017-01-17 14:58:28 -0500

    add * in front of each item in grocery_list

---
+++ exercise6.rb
@@ -0,0 +1,8 @@
+#Exercise 6 - Grocer List
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+# puts "*" in front of each item in the grocery_list
+grocery_list.each do |grocery_list2|
+  puts "* #{grocery_list2}"
+end
