d3107af0c7ce2e06e86c65314e08f2568ff05b66
wtung008 <wayne.h.tung@gmail.com>
2017-01-17 15:21:04 -0500

    Add steps 1 - creating method and add new item

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,22 @@
-#Exercise 6 - Grocer List
+#Exercise 6 - Grocery List
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-# puts "*" in front of each item in the grocery_list
-grocery_list.each do |grocery_list2|
-  puts "* #{grocery_list2}"
+# Step 1 puts "*" in front of each item in the grocery_list
+#grocertey_list_final = grocery_list.each do |grocery_list2|
+#  puts "* #{grocery_list2}"
+#end
+
+# Step 1(a) add rice to grocery List
+grocery_list << "rice"
+
+
+# Step 1b - Method for adding * in front of each item in the list
+def grocery_list_final (a)
+  a.each do |grocery_list2|
+    puts "* #{grocery_list2}"
+  end
 end
+
+# Print final list
+grocery_list_final(grocery_list)
