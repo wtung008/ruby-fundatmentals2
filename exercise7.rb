480df196660d584d832ddb33cbddb5bb3f10445f
wtung008 <wayne.h.tung@gmail.com>
2017-01-17 18:42:10 -0500

    add steps 5 and 7 to exercise7 file

--- exercise7.rb
+++ exercise7.rb
@@ -21,15 +21,24 @@ students[:cohort4] = 43
 # Step 4: Keys method to output all of the cohort names
 students.each do |k, v|
   puts "#{k}"
+  # students[k] = v * 1.05
 end
 
 
 # Step 5: increase class size by 5%
-#students.keys.each do |k|
-#  k * 1.05
-#end
+students.each do |k, v|
+  students[k] = v * 1.05
+end
 
 
 # Step 6: Delete 2nd cohort
 students.delete(:cohort2)
 cohorts(students)
+
+
+# Step 7: Total number of students
+student_totals = 0
+students.each do |k, v|
+  student_totals += v
+end
+puts "Total number of students #{student_totals}"
